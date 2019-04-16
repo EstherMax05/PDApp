9ef6f2eb7efced39d2a24cd1f86bbad9421e01ab
netrecon <netrecon@inemas-mac-pro.local>
2019-04-16 19:40:50 -0400

    Initial Commit

--- PDApp/PickerViewController.swift
+++ PDApp/PickerViewController.swift
@@ -6,25 +6,48 @@
 //  Copyright © 2019 netrecon. All rights reserved.
 //
 
+
 import UIKit
-
-class PickerViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class PickerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        print(pickerData.count)
+        return pickerData.count
+    }
+    
+    /*func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        print(pickerData[row])
+        return pickerData[row]
+    }*/
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let string = pickerData[row]
+        return NSAttributedString(string: string, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        pickerData = ["Caucasian", "African American", "Hispanic", "Asian", "Native American", "Other"]
+        picker.delegate = self
+        picker.dataSource = self
+        
+        
+        
+    }
+    
+    
 }
